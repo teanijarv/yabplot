8947b3101f8193ff4b61ee4ecc0e8f6a6ee1b62d
Toomas Erik Anijärv <toomaserikanijarv@gmail.com>
2026-01-13 22:01:08 +0100

    version

--- yabplot/__init__.py
+++ yabplot/__init__.py
@@ -1,2 +1,9 @@
+from importlib.metadata import version, PackageNotFoundError
+
 from .plotting import plot_cortical, plot_subcortical, plot_tracts, clear_tract_cache
 from .data import get_available_resources, get_atlas_regions
+
+try:
+    __version__ = version("yabplot")
+except PackageNotFoundError:
+    __version__ = "unknown"
