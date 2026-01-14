4e4567f3e092dd2a39b74435afd86090aee579c3
Toomas Erik Anijärv <toomaserikanijarv@gmail.com>
2026-01-14 15:06:45 +0100

    tests offscreen fix

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -23,16 +23,16 @@ def test_plot_cortical():
     """
     Integration test: Downloads 'aparc' and plots it.
     """
-    yab.plot_cortical(atlas='aparc')
+    yab.plot_cortical(atlas='aparc', display_type='none')
 
 def test_plot_subcortical():
     """
     Integration test: Downloads 'aseg' and plots it.
     """
-    yab.plot_subcortical(atlas='aseg')
+    yab.plot_subcortical(atlas='aseg', display_type='none')
 
 def test_plot_tracts():
     """
     Integration test: Downloads 'xtract_tiny' and plots it.
     """
-    yab.plot_tracts(atlas='xtract_tiny')
+    yab.plot_tracts(atlas='xtract_tiny', display_type='none')
